632547f06de7008f6844e53bacdbf77d9ff96cf2
Pimentel <EPIMENT5@ford.com>
2021-11-23 15:35:54 -0300

    funcao deleteItem adicionada ao ViewModel (InventoryViewModel) e utilizacao do ConfirmationDialog apos deletar o item

--- app/src/main/java/com/example/inventory/ItemDetailFragment.kt
+++ app/src/main/java/com/example/inventory/ItemDetailFragment.kt
@@ -74,7 +74,8 @@ class ItemDetailFragment : Fragment() {
             binding.itemCount.text = item.quantityInStock.toString()
 
             sellItem.isEnabled = viewModel.isStockAvailable(item)
-            sellItem.setOnClickListener{viewModel.sellItem(item)}
+            sellItem.setOnClickListener{viewModel.sellItem(item) }
+            deleteItem.setOnClickListener { showConfirmationDialog() }
         }
     }
 
@@ -97,6 +98,7 @@ class ItemDetailFragment : Fragment() {
      * Deletes the current item and navigates to the list fragment.
      */
     private fun deleteItem() {
+        viewModel.deleteItem(item)
         findNavController().navigateUp()
     }
 
