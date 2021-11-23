60cccce2b8a34c6a88befa6f24bd01469e4ce328
Pimentel <EPIMENT5@ford.com>
2021-11-23 12:28:06 -0300

    funcao update adicionada

--- app/src/main/java/com/example/inventory/data/ItemDao.kt
+++ app/src/main/java/com/example/inventory/data/ItemDao.kt
@@ -6,15 +6,20 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface ItemDao {
 
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insert(item: Item)
-
-    @Delete
-    suspend fun delete(item: Item)
+    @Query("SELECT * from item ORDER BY name ASC")
+    fun getItems(): Flow<List<Item>>
 
     @Query("SELECT * from item WHERE id = :id")
     fun getItem(id: Int): Flow<Item>
 
-    @Query("SELECT * from item ORDER BY name ASC")
-    fun getItems(): Flow<List<Item>>
+    // Specify the conflict strategy as IGNORE, when the user tries to add an
+    // existing Item into the database.
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(item: Item)
+
+    @Update
+    suspend fun update(item: Item)
+
+    @Delete
+    suspend fun delete(item: Item)
 }
